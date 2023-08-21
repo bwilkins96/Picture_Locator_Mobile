80999da1e5afc373a80ae930d55f0fd2b11a74b8
bwilkins96 <ben.jr.speed@gmail.com>
2023-08-21 14:19:32 -0400

    coordinates

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
-import { Camera, CameraResultType } from '@capacitor/camera';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-tab2',
@@ -15,13 +16,35 @@ export class Tab2Page {
 
   constructor() {}
 
-  async addPicture() {
-    const image = await Camera.getPhoto({
-      quality: 90,
-      allowEditing: true,
-      resultType: CameraResultType.Uri
-    });
+  async getPicture() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera
+      });
+      
+      return image;
+    } catch {
+      return false;
+    }
+  }
 
+  async getCoords() {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      return position.coords; 
+    } catch {
+      return false;
+    }
+  }
+
+  async addPicture() {
+    const image = await this.getPicture();    
+    const coordinates = await this.getCoords();
+    
     console.log(image);
+    console.log(coordinates);
   }
 }
