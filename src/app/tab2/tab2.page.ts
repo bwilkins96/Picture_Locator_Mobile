36231a48172856ab39ad387759321b4042b3f501
bwilkins96 <ben.jr.speed@gmail.com>
2023-08-21 16:53:29 -0400

    reverse geocoding / major functionality

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { CommonModule, PathLocationStrategy } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
@@ -9,14 +9,50 @@ import { Geolocation, Position } from '@capacitor/geolocation';
 class AppPicture {
   picture: Photo;
   coords: Position['coords'];
+  location: any;
 
   constructor(picture: Photo, coords: Position['coords']) {
     this.picture = picture;
     this.coords = coords;
+    
+    this.reverseGeocode().then(res => {
+      this.location = res;
+    });
+  }
+
+  private async reverseGeocode() {
+    const [lat, long] = this.getLatLong();
+
+    try {
+      const res = await fetch(`https://geocode.maps.co/reverse?lat=${lat}&lon=${long}`);
+      const result = await res.json();
+
+      return result.address;
+    } catch {
+      return null;
+    }
   }
 
   getLatLong() {
-    return `(${this.coords.latitude}, ${this.coords.longitude})`;
+    return [this.coords.latitude, this.coords.longitude];
+  }
+
+  getLocationString() {
+    const loc = this.location;
+    let locStr: string;
+
+    console.log(loc);
+
+    if (loc) {
+      locStr = `${loc.road}, ${loc.county}, ${loc.state}, ${loc.postcode}, ${loc.country_code.toUpperCase()}`;
+    } else if (this.coords) {
+      const [lat, long] = this.getLatLong();
+      locStr = `(${lat}, ${long})`;
+    } else {
+      locStr = 'Location information unavailable'
+    }
+
+    return locStr;
   }
 }
 
