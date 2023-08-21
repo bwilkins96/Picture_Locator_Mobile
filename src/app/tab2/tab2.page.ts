a3215d30098616ac3b6f01038270cddfd1fb8e58
bwilkins96 <ben.jr.speed@gmail.com>
2023-08-21 13:33:34 -0400

    working on camera

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
+import { Camera, CameraResultType } from '@capacitor/camera';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -13,4 +15,13 @@ export class Tab2Page {
 
   constructor() {}
 
+  async addPicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+
+    console.log(image);
+  }
 }
